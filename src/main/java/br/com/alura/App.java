41c179cc7738f9d81ff09da2d795d3793da055c9
Eliezer de Almeida <20017826@uepg.br>
2024-12-10 09:47:53 -0300

    add app

---
+++ src/main/java/br/com/alura/App.java
@@ -0,0 +1,14 @@
+package br.com.alura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Conversor cv = new Conversor();
+        cv.app();
+    }
+}
